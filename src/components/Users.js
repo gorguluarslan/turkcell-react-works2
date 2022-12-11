f9496f66b22afafefdd77fd5da2f0146365b68c2
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2022-12-11 15:59:29 +0300

    update: add axios catch function

--- src/components/Users.js
+++ src/components/Users.js
@@ -7,30 +7,36 @@ const Users = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // axios("https://jsonplaceholder.typicode.com/users")
-    //   .then((res) => {
-    //     setUsers(res.data);
-
-    //     axios(
-    //       `https://jsonplaceholder.typicode.com/posts?userId=${res.data[0].id}`
-    //     ).then((res) => setPosts(res.data));
-    //   })
-    //   .finally(() => setLoading(false));
-    getData();
+    axios("https://jsonplaceholder.typicode.com/usersf")
+      .then((res) => {
+        setUsers(res.data);
+        axios(
+          `https://jsonplaceholder.typicode.com/posts?userId=${res.data[0].id}`
+        ).then((res) => setPosts(res.data));
+      })
+      .catch((error) => {
+        console.log("error", error);
+      })
+      .finally(() => setLoading(false));
+    //getData();
   }, []);
 
-  const getData = async () => {
-    const { data: users } = await axios(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    const { data: posts } = await axios(
-      `https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`
-    );
+  // const getData = async () => {
+  //  try {
+  //   const { data: users } = await axios(
+  //     "https://jsonplaceholder.typicode.com/usersf"
+  //   );
+  //   const { data: posts } = await axios(
+  //     `https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`
+  //   );
 
-    setLoading(false);
-    setUsers(users);
-    setPosts(posts);
-  };
+  //   setLoading(false);
+  //   setUsers(users);
+  //   setPosts(posts);
+  //  } catch(error) => {
+  //    console.log("error",error);
+  //  }
+  // };
 
   return (
     <div>
