36b3615359f166b1f2bc2e229c37933c3704062e
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2022-12-11 16:04:10 +0300

    update: use try catch function

--- src/components/Users.js
+++ src/components/Users.js
@@ -7,36 +7,36 @@ const Users = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios("https://jsonplaceholder.typicode.com/usersf")
-      .then((res) => {
-        setUsers(res.data);
-        axios(
-          `https://jsonplaceholder.typicode.com/posts?userId=${res.data[0].id}`
-        ).then((res) => setPosts(res.data));
-      })
-      .catch((error) => {
-        console.log("error", error);
-      })
-      .finally(() => setLoading(false));
-    //getData();
+    // axios("https://jsonplaceholder.typicode.com/usersf")
+    //   .then((res) => {
+    //     setUsers(res.data);
+    //     axios(
+    //       `https://jsonplaceholder.typicode.com/posts?userId=${res.data[0].id}`
+    //     ).then((res) => setPosts(res.data));
+    //   })
+    //   .catch((error) => {
+    //     console.log("error", error);
+    //   })
+    //   .finally(() => setLoading(false));
+    getData();
   }, []);
 
-  // const getData = async () => {
-  //  try {
-  //   const { data: users } = await axios(
-  //     "https://jsonplaceholder.typicode.com/usersf"
-  //   );
-  //   const { data: posts } = await axios(
-  //     `https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`
-  //   );
+  const getData = async () => {
+    try {
+      const { data: users } = await axios(
+        "https://jsonplaceholder.typicode.com/usersf"
+      );
+      const { data: posts } = await axios(
+        `https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`
+      );
 
-  //   setLoading(false);
-  //   setUsers(users);
-  //   setPosts(posts);
-  //  } catch(error) => {
-  //    console.log("error",error);
-  //  }
-  // };
+      setLoading(false);
+      setUsers(users);
+      setPosts(posts);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   return (
     <div>
